c48fd1be3418551ddcec49ad73eab5197378e07b
sstokesgmu <sterlingjstokes@gmail.com>
2025-06-17 00:22:53 -0400

    Added during meet up should be some quick edits

--- main.ts
+++ main.ts
@@ -16,7 +16,6 @@ export default class AIAnnotatorPlugin extends Plugin {
 			name: "Convert Note to Plain Text",
 			callback: () => this.convertNoteToPlainText(),
 		});
-
 		console.log("AI Annotator Plugin Loaded!");
 	}
 
@@ -30,6 +29,8 @@ export default class AIAnnotatorPlugin extends Plugin {
 		//ToDo: get a list of all headings in the note and list them out in the modal
 		const parser = new Parser(markdownContent);
 		const sections = parser.getHeadings();
+
+		//? What is returned from this fuction
 		const func = async (sections): Promise<any> => {
 			return new Promise((resolve) => {
 				const modal = new DropDownModal(this.app, sections, resolve);
@@ -38,37 +39,28 @@ export default class AIAnnotatorPlugin extends Plugin {
 		};
 		const bounds = await func(sections);
 		console.log(bounds);
-		const response = await this.sendToOpenAI(parser.getContent(bounds));
-		console.log(response);
-		// organize the response in a  side bar that I can use
+		const response: string | undefined = await this.sendToOpenAI(
+			parser.getContent(bounds),
+		);
+		//
+		//TODO: Organize the response in a  side bar that I can use
 		await this.createNewNote(response);
 		new Notice("Conversion to plain text completed!");
-
-		
 	}
-
-	// // Helper Method to prompt the user for input using Obsidian's modal
-	// async promptUser(message: string, defaultValue: string): Promise<string> {
-	// 	return new Promise((resolve) => {
-	// 		const modal = new InputModal(
-	// 			this.app,
-	// 			message,
-	// 			defaultValue,
-	// 			resolve
-	// 		);
-	// 		modal.open();
-	// 	});
-	// }
-
 	async sendToOpenAI(text: any) {
 		try {
-			const response = await fetch('https://obsidian-plugin-server.onrender.com/',
+			const response = await fetch(
+				"https://obsidian-plugin-server.onrender.com/",
 				{
-					method:'POST',
-					body:text,
-				}
-			)
-			return await response.text();
+					method: "POST",
+					body: text,
+				},
+			);
+			new Notice("Sending Response ...");
+			const thing = await response.text();
+			//? Is there an possible exception here?
+			new Notice("Respose Returned!");
+			return thing;
 		} catch (error) {
 			console.error("Error communicating with OpenAI", error);
 			new Notice("Error during AI review");
@@ -89,38 +81,45 @@ export default class AIAnnotatorPlugin extends Plugin {
 
 		try {
 			// Ensure the folder exists
-			const folderExists = await this.app.vault.adapter.exists(folderPath);
+			const folderExists =
+				await this.app.vault.adapter.exists(folderPath);
 			if (!folderExists) {
 				// If folder doesn't exist, create it
 				await this.app.vault.createFolder(folderPath);
 			}
-			
+
 			// Create the new file with the given content
 			await this.app.vault.create(filePath, markdownContent);
-    
+
 			// Get the active file in the workspace
 			const activeFile = this.app.workspace.getActiveFile();
-			
+
 			// Get the file object for the newly created file
 			const newFileObj = this.app.vault.getAbstractFileByPath(filePath);
-		
+
 			if (newFileObj instanceof TFile) {
 				// Append a link to the active file in the newly created file
-				await this.app.vault.append(newFileObj, `\n\n# Document Link:\n[[${activeFile?.name}]]`);
+				await this.app.vault.append(
+					newFileObj,
+					`\n\n# Document Link:\n[[${activeFile?.name}]]`,
+				);
 			} else {
-				new Notice('Error: New file could not be found.');
+				new Notice("Error: New file could not be found.");
 			}
-		
+
 			// Get the current active file (if it exists) and append content
 			if (activeFile instanceof TFile) {
-				await this.app.vault.append(activeFile, `\n\n## App Review: \n[[${newFileObj?.name}]]`);
+				await this.app.vault.append(
+					activeFile,
+					`\n\n## App Review: \n[[${newFileObj?.name}]]`,
+				);
 			} else {
-				new Notice('Error: Active file could not be found.');
+				new Notice("Error: Active file could not be found.");
 			}
-		
+
 			// Show a notice to the user
 			new Notice(`New note created: ${filePath}`);
-		
+
 			//Todo Add the backlink to the current file
 		} catch (error) {
 			console.error("Error creating note:", error);
@@ -171,4 +170,16 @@ export default class AIAnnotatorPlugin extends Plugin {
 // 			this.close();
 // 		});
 // 	}
+// }
+// // Helper Method to prompt the user for input using Obsidian's modal
+// async promptUser(message: string, defaultValue: string): Promise<string> {
+// 	return new Promise((resolve) => {
+// 		const modal = new InputModal(
+// 			this.app,
+// 			message,
+// 			defaultValue,
+// 			resolve
+// 		);
+// 		modal.open();
+// 	});
 // }
